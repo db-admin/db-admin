a1313f7d080b8dbae3e96eb5f27363730cdbca8a
Yousef Shanawany <askyous@gmail.com>
2017-01-01 14:21:02 -0800

    Created button and styled

--- api/models/Class.js
+++ api/models/Class.js
@@ -14,6 +14,7 @@
        'collection': 'session', // collection: <table name>
        'via': 'classes',
        'through': 'classsession'
-     }
+     },
+     'supportedMajors': { 'collection': 'major', 'via': 'requiredClasses'}
    }
  };
