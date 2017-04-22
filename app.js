81ad4f3f599cd8f08970a95a34899a5007a85e49
Yousef Shanawany <askyous@gmail.com>
2017-04-22 17:01:57 -0400

    Typescript setup.

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-// Ensure we're in the project directory, so cwd-relative paths work as expected
+// ensure we're in the project directory, so cwd-relative paths work as expected
 // no matter where we actually lift from.
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
-
-const sails = require('sails');
-let rc = require('rc');
+var sails = require('sails');
+var rc = require('rc');
 rc = rc('sails');
-
 // Sails My Admin
-const fs = require('fs');
-const config = JSON.parse(fs.readFileSync('sma.config.json'));
-
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('sma.config.json'));
 rc.connections = {
-  'userSpecified': config.connections[config.connection]
+    'userSpecified': config.connections[config.connection]
 };
 rc.globals = {
-  'title': config.title
+    'title': config.title
 };
-
-sails.lift(rc, error => {
-  if(error) return console.error(error);
-  console.log('Ready at localhost:1337');
+sails.lift(rc, function (error) {
+    if (error)
+        return console.error(error);
+    console.log('Ready at localhost:1337');
 });
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,24 @@
+// ensure we're in the project directory, so cwd-relative paths work as expected
+// no matter where we actually lift from.
+// > Note: This is not required in order to lift, but it is a convenient default.
+process.chdir(__dirname);
+
+const sails = require('sails');
+let rc = require('rc');
+rc = rc('sails');
+
+// Sails My Admin
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('sma.config.json'));
+
+rc.connections = {
+  'userSpecified': config.connections[config.connection]
+};
+rc.globals = {
+  'title': config.title
+};
+
+sails.lift(rc, error => {
+  if (error) return console.error(error);
+  console.log('Ready at localhost:1337');
+});
