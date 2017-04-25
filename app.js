35e918292dd8b6da85a5ead81eb9d1ac2c353042
Yousef Shanawany <askyous@gmail.com>
2017-04-24 20:08:25 -0700

    Fixed app.ts tslints

--- app.js
+++ app.js
@@ -2,21 +2,22 @@
 // no matter where we actually lift from.
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
-var sails = require('sails');
-var rc = require('rc');
-rc = rc('sails');
-// Sails My Admin
-var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('sma.config.json'));
+var sails = require("sails");
+var rc = require("rc");
+rc = rc("sails");
+// sails My Admin
+var fs = require("fs");
+var config = JSON.parse(fs.readFileSync("sma.config.json"));
 rc.connections = {
-    'userSpecified': config.connections[config.connection]
+    "userSpecified": config.connections[config.connection]
 };
 rc.globals = {
-    'title': config.title
+    "title": config.title
 };
 sails.lift(rc, function (error) {
-    if (error)
+    if (error) {
         return console.error(error);
-    console.log('Ready at localhost:1337');
+    }
+    console.log("Ready at localhost:1337");
 });
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -3,22 +3,24 @@
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
 
-const sails = require('sails');
-let rc = require('rc');
-rc = rc('sails');
+const sails: any = require("sails");
+let rc: any = require("rc");
+rc = rc("sails");
 
-// Sails My Admin
-const fs = require('fs');
-const config = JSON.parse(fs.readFileSync('sma.config.json'));
+// sails My Admin
+const fs: any = require("fs");
+const config: any = JSON.parse(fs.readFileSync("sma.config.json"));
 
 rc.connections = {
-  'userSpecified': config.connections[config.connection]
+  "userSpecified": config.connections[config.connection]
 };
 rc.globals = {
-  'title': config.title
+  "title": config.title
 };
 
 sails.lift(rc, error => {
-  if (error) return console.error(error);
-  console.log('Ready at localhost:1337');
+  if (error) {
+    return console.error(error);
+  }
+  console.log("Ready at localhost:1337");
 });
