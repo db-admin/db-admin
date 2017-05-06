0aebd7fd10dfffa4cd73aae2edc8e9807973c61e
Son Nguyen <soullessknight@gmail.com>
2017-05-06 07:01:51 -0700

    should pass

--- spec/hello_world_spec.js
+++ spec/hello_world_spec.js
@@ -2,8 +2,8 @@ describe("Hello World Test", function () {
     it("should be true", function () {
         expect(true).toBe(true);
     });
-    it("should fail", function () {
-        expect(false).toBe(true);
-    });
+    // it("should fail", function () {
+    //     expect(false).toBe(true);
+    // });
 
 });
