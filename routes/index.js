78e6b0ed48d862f3ddb0c9c76ea47e19293959ab
Yousef Shanawany <askyous@gmail.com>
2017-09-10 14:27:57 -0700

    Homepage kinda works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 "use strict";
+require("sequelize");
 const express = require("express");
+const DBAConfig = require("../dba.config");
+const models = require("../models/db.tables");
+const Sequelize = require("sequelize");
+const dbConfig = DBAConfig.connections[DBAConfig.connection];
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    dialectOptions: { ssl: true },
+    define: { freezeTableName: true }
+});
 const router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Exdsspress" });
+    res.render("index", { title: DBAConfig.title, tables: Object.keys(models.getModels(sequelize)) });
 });
 module.exports = router;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,22 @@
 import * as Express from "express";
-
+import "sequelize";
 import express = require("express");
+import DBAConfig = require("../dba.config");
+import models = require("../models/db.tables");
+import Sequelize = require("sequelize");
+
+const dbConfig = DBAConfig.connections[DBAConfig.connection];
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect,
+  dialectOptions: { ssl: true },
+  define: { freezeTableName: true }
+});
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req: Express.Request, res: Express.Response, next: Express.Handler) {
-  res.render("index", { title: "Exdsspress" });
+  res.render("index", { title: DBAConfig.title, tables: Object.keys(models.getModels(sequelize)) });
 });
 
 export = router;
